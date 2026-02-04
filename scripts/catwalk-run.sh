ce777617bb8b4a8de6093dc141cf27a283a90ab3
Scarce Koi <scarcekoi@gmail.com>
2026-02-04 01:00:52 -0500

    feat(assets): add assets

---
+++ scripts/catwalk-run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+FLAGS_FILE="$SCRIPT_DIR/../resources/flags.yml"
+
+FLAGS=$(python3 - <<PYTHON
+import yaml
+with open("$FLAGS_FILE") as f:
+    data = yaml.safe_load(f)
+flags = data.get("flags", {})
+print(" ".join(flags.keys()))
+PYTHON
+)
+
+LAYOUTS="composite grid row stacked"
+
+for flag in $FLAGS; do
+    for layout in $LAYOUTS; do
+        catwalk \
+            "themes/latte/$flag/$flag.webp" \
+            "themes/frappe/$flag/$flag.webp" \
+            "themes/macchiato/$flag/$flag.webp" \
+            "themes/mocha/$flag/$flag.webp" \
+            -o "assets/$layout/$flag.webp" \
+            -l "$layout" -r 0
+    done
+done
